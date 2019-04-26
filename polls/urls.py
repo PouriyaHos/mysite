fe2c39cebc0b424f02fcdcef850c5fe158728699
pouriya91 <50017735+pouriya91@users.noreply.github.com>
2019-04-26 19:48:25 +0200

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
 from . import views
+from django.urls import path
+
 
 app_name = 'polls'
 urlpatterns = [
@@ -8,4 +9,3 @@ urlpatterns = [
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
-
