4fa3d0d61c8ed66240b80f6b619c24466bc67109
pouriya91 <50017735+pouriya91@users.noreply.github.com>
2019-04-26 19:47:16 +0200

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -1,33 +1,35 @@
-from django.db import models
 import datetime
+
+from django.db import models
+from django.db import models
 from django.utils import timezone
+from django.db import models
 
 
 class Question(models.Model):
-    # ...
+     # ...
     def __str__(self):
         return self.question_text
-
-class Choice(models.Model):
-    # ...
-    def __str__(self):
-        return self.choice_text
-
-
-
-
-class Question(models.Model):
-    # ...
+         # ...
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+ 
+
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
 
 
+class Choice(models.Model):
+       # ...
+    def __str__(self):
+        return self.choice_text
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
 
-class Question(models.Model):
-    # ...
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
+    from django.db import models
+
+
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
